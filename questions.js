9dd2c182a65c9621ded77f5c72f8ce834f66917f
RafaelIIIPrudente <97773959+RafaelIIIPrudente@users.noreply.github.com>
2022-01-15 23:26:47 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+  {
+  numb: 1,
+  question: "What does HTML stand for?",
+  answer: "b. Hyper Text Markup Language",
+  options: [
+    "a. Hyper Text Preprocessor",
+    "b. Hyper Text Markup Language",
+    "c. Hyper Text Multiple Language",
+    "d. Hyper Tool Multi Language"
+  ]
+},
+  {
+  numb: 2,
+  question: "How many tags are in a regular element?",
+  answer: "b. 2",
+  options: [
+    "a. 1",
+    "b. 2",
+    "c. 3",
+    "d. 4"
+  ]
+},
+  {
+  numb: 3,
+  question: "Choose the correct HTML element for the largest heading:", 
+  answer: "d. h1",
+  options: [
+    "a. h6",
+    "b. heading",
+    "c. head",
+    "d. h1"
+  ]
+},
+  {
+  numb: 4,
+  question: "Where is the meta tag only found",
+  answer: "c. The home page",
+  options: [
+    "a. The last page",
+    "b. The second page",
+    "c. The home page",
+    "d. None of the Above"
+  ]
+},
+  {
+  numb: 5,
+  question: "What is an element that does not have a closing tag called?",
+  answer: "b. Empty element",
+  options: [
+    "a. Tag",
+    "b. Empty element",
+    "c. None of the choices",
+    "d. Closed element"
+  ]
+},
+  {
+  numb: 6,
+  question: "Which character is used to indicate an end tag?",
+  answer: "b. /",
+  options: [
+    "a. *",
+    "b. /",
+    "c. <",
+    "d. `"
+  ]
+},
+  {
+  numb: 7,
+  question: "Which of the following is the correct syntax to display “GeeksforGeeks” in an alert box using JavaScript?",
+  answer: "d. alert(“GeeksforGeeks”);",
+  options: [
+    "a. alertbox(“GeeksforGeeks”);",
+    "b. msgbox(“GeeksforGeeks”);",
+    "c. msg(“GeeksforGeeks”);",
+    "d. alert(“GeeksforGeeks”);"
+  ]
+},
+  {
+  numb: 8,
+  question: "Which is the correct syntax for displaying data in the console?",
+  answer: "a. console.log();",
+  options: [
+    "a. console.log();",
+    "b. console.log[];",
+    "c. log.console();",
+    "d. console.log;"
+  ]
+},
+  {
+  numb: 9,
+  question: "What is the correct syntax for declaring a function?",
+  answer: "b. function myFunction()",
+  options: [
+    "a. function my Function()",
+    "b. function myFunction()",
+    "c. myFunction function()",
+    "d. var myFunction()"
+  ]
+},
+  {
+  numb: 10,
+  question: "In JavaScript, what element is used to store and manipulate text usually in multiples?",
+  answer: "d. Arrays",
+  options: [
+    "a. Function",
+    "b. Variables",
+    "c. Strings",
+    "d. Arrays"
+  ]
+},
+];
